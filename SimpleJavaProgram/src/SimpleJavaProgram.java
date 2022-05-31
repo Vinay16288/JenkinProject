af4660b39deb0368c7ca527c69b6cbadb0567d22
vitiwari <vitiwari@AIPL3136LT15334.altimetrik.com>
2022-05-31 16:38:18 +0530

    add

--- SimpleJavaProgram/src/SimpleJavaProgram.java
+++ SimpleJavaProgram/src/SimpleJavaProgram.java
@@ -4,7 +4,6 @@ public class SimpleJavaProgram {
 	public static void main(String[] args) {
 		
 		System.out.println("My first program with Git Repository");
-		
-		System.out.println("My first program with Git Repository");
+	
 	}
 }
